7f3c04d2192c10e666f61d5066df3fda1a91ecba
Walter Hoerman <whoerman@gmail.com>
2020-02-06 13:41:52 -0500

    reverting back to previous

--- Mainjs.js
+++ Mainjs.js
@@ -33,7 +33,7 @@ function getStorage() {
     } else {
         greetingMessage();
     };
-
+    
 };
 getStorage();
 
@@ -68,9 +68,9 @@ function newCity() {
 };
 
 //putting the current city data on the page
-function todayCityData() {
+function todayCityData () {
 
-    //putting the top date on the page
+ /*   //putting the top date on the page
     datePlace();
     //putting the city on the ag{}
     $(".cityTitle").text(currentPick);
@@ -83,110 +83,92 @@ function todayCityData() {
     //putting the latitude and longitude on the page
     $(".cityCoord").text(`Current Data:  (latitute: ${lat2} & longitude: ${lng2})`)
     //processing the temp info and putting on page
-    let currentTemp = responseWeather.main.temp;
-    //changing from Kelvin (0K − 273.15) × 9/5 + 32 = -459.7°F
-    currentTemp = currentTemp - 273.15;
-    currentTemp = currentTemp * 9;
-    currentTemp = currentTemp / 5;
-    currentTemp = currentTemp + 32;
-    currentTemp1 = currentTemp.toFixed(1);
-    $(".currentTempText").text(`Temperature:  ${currentTemp1}°`);
+    let currentTemp = responseWeather.main.temp; 
+        //changing from Kelvin (0K − 273.15) × 9/5 + 32 = -459.7°F
+        currentTemp = currentTemp - 273.15;
+        currentTemp = currentTemp * 9;
+        currentTemp = currentTemp / 5;
+        currentTemp = currentTemp + 32;
+        currentTemp1 = currentTemp.toFixed(1);
+        $(".currentTempText").text(`Temperature:  ${currentTemp1}°`);
     //getting the wind speed and putting it on the page
     let currentWind = responseWeather.wind.speed;
     $(".currentwindSpeedText").text(`Wind Speed:   ${currentWind} mph`);
-
+*/
 };
 
 
 
 //API Functions
 
-//getting latitude and logitude from google API
-function locationGet() {
-    let queryURLG = `https://maps.googleapis.com/maps/api/geocode/json?address=${currentPick}&key=AIzaSyCRTF7l44EF1KpczSGOLsMu5jewKiBDNW0`
+    //getting latitude and logitude from google API
+    function locationGet() {
+        let queryURLG = `https://maps.googleapis.com/maps/api/geocode/json?address=${currentPick}&key=AIzaSyCRTF7l44EF1KpczSGOLsMu5jewKiBDNW0`
 
-    $.ajax({
-            url: queryURLG,
-            method: "GET"
-        })
-        .then(function (responseLocation) {
-            responseLocationObj = responseLocation;
-            console.log(responseLocation);
+        $.ajax({
+                url: queryURLG,
+                method: "GET"
+            })
+            .then(function (responseLocation) {
+                responseLocationObj = responseLocation;
+                console.log(responseLocation);
 
-            //finding the geocoordinates in the response
-            latitude = responseLocation.results[0].geometry.location.lat;
-            lat2 = latitude.toFixed(2);
-            longitude = responseLocation.results[0].geometry.location.lng;
-            lng2 = longitude.toFixed(2);
+                //finding the geocoordinates in the response
+                latitude = responseLocation.results[0].geometry.location.lat;
+                lat2 = latitude.toFixed(2);
+                longitude = responseLocation.results[0].geometry.location.lng;
+                lng2 = longitude.toFixed(2);
 
-            //run weather gets inside this because need the geodata
-            let queryURLWeather = `http://api.openweathermap.org/data/2.5/weather?lat=${lat2}&lon=${lng2}&APPID=56657235d578c7d8b7f0b5ce07c9c887`
-            console.log("second AJAX");
-            console.log(`Current Data:  (latitute: ${lat2} & longitude: ${lng2})`);
-            $.ajax({
-                    url: queryURLWeather,
-                    method: "GET"
-                })
-                .then(function (responseWeather) {
-                    responseWeatherObj = responseWeather
-                    console.log(responseWeather);
-                    //putting the top date on the page
-                    datePlace();
-                    //putting the city on the ag{}
-                    $(".cityTitle").text(currentPick);
-                    //getting the corect icon from openweathermap and adding it on cityTitle
-                    let currentIcon = responseWeather.weather[0].icon;
-                    let iconURL = `http://openweathermap.org/img/wn/${currentIcon}@2x.png`
-                    let $titleImage = $("<img>");
-                    $titleImage.attr("src", iconURL);
-                    $(".cityTitle").append($titleImage);
-                    //putting the latitude and longitude on the page
-                    $(".cityCoord").text(`Current Data:  (latitute: ${lat2} & longitude: ${lng2})`)
-                    //processing the temp info and putting on page
-                    let currentTemp = responseWeather.main.temp;
-                    //changing from Kelvin (0K − 273.15) × 9/5 + 32 = -459.7°F
-                    currentTemp = currentTemp - 273.15;
-                    currentTemp = currentTemp * 9;
-                    currentTemp = currentTemp / 5;
-                    currentTemp = currentTemp + 32;
-                    currentTemp1 = currentTemp.toFixed(1);
-                    $(".currentTempText").text(`Temperature:  ${currentTemp1}°`);
-                    //getting the wind speed and putting it on the page
-                    let currentWind = responseWeather.wind.speed;
-                    $(".currentwindSpeedText").text(`Wind Speed:   ${currentWind} mph`);
-                });
-        });
+                //run weather gets inside this because need the geodata
+                openWeatherGet();
+                openForecastGet();
+                openUVIGet();
+            });
 
-};
+    };
 
+    //using lat and long to get current weather
+    function openWeatherGet() {
+        let queryURLWeather = `http://api.openweathermap.org/data/2.5/weather?lat=${lat2}&lon=${longitude}&APPID=56657235d578c7d8b7f0b5ce07c9c887`
+        console.log("second AJAX");
+        console.log(`Current Data:  (latitute: ${lat2} & longitude: ${lng2})`);
+        $.ajax({
+                url: queryURLWeather,
+                method: "GET"
+            })
+            .then(function (responseWeather) {
+                responseWeatherObj = responseWeather
+                console.log(responseWeather);
+            });
+    };
 
-//using lat and long to get 5 day forecast
-function openForecastGet() {
-    let queryURLForecast = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&APPID=56657235d578c7d8b7f0b5ce07c9c887`
+    //using lat and long to get 5 day forecast
+    function openForecastGet() {
+        let queryURLForecast = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&APPID=56657235d578c7d8b7f0b5ce07c9c887`
 
-    $.ajax({
-            url: queryURLWeather,
-            method: "GET"
-        })
-        .then(function (responseForecast) {
-            responseForecastObj = responseForecast;
-            console.log(responseForecast);
-        });
-};
+        $.ajax({
+                url: queryURLWeather,
+                method: "GET"
+            })
+            .then(function (responseForecast) {
+                responseForecastObj = responseForecast;
+                console.log(responseForecast);
+            });
+    };
 
-//using lat and long to get 5 day forecast
-function openUVIGet() {
-    let queryURLUVI = `http://api.openweathermap.org/data/2.5/uvi?lat=${latitude}&lon=${longitude}&APPID=56657235d578c7d8b7f0b5ce07c9c887`
+    //using lat and long to get 5 day forecast
+    function openUVIGet() {
+        let queryURLUVI = `http://api.openweathermap.org/data/2.5/uvi?lat=${latitude}&lon=${longitude}&APPID=56657235d578c7d8b7f0b5ce07c9c887`
 
-    $.ajax({
-            url: queryURLUVI,
-            method: "GET"
-        })
-        .then(function (responseUVI) {
-            responseUVIObj = responseUVI;
-            console.log(responseUVI);
-        });
-};
+        $.ajax({
+                url: queryURLUVI,
+                method: "GET"
+            })
+            .then(function (responseUVI) {
+                responseUVIObj = responseUVI;
+                console.log(responseUVI);
+            });
+    };
 
 //erases the cityInput array and removes buttons from page
 function eraseButton() {
@@ -199,16 +181,16 @@ function eraseButton() {
 //listen for button input and the magic starts
 $(document).on("click", function () {
 
-    $("#eraseCities").on("click", function () {
-        eraseButton();
+            $("#eraseCities").on("click", function () {
+                    eraseButton();
 
-    });
+                });
 
-    $(".cityPickBtn").on("click", function () {
-        currentPick = $(".cityPickInfo").val();
-        locationGet();
-        newCity();
-        todayCityData();
+                $(".cityPickBtn").on("click", function () {
+                    currentPick = $(".cityPickInfo").val();
+                    locationGet();
+                    newCity();
+                    todayCityData();
 
-    });
-});
+                });
+            });
--- main.js
+++ main.js
@@ -47,7 +47,6 @@ $(document).ready(function () {
 
     //erases the cityInput array and removes buttons from page
     $("#eraseCities").on("click", function () {
-        refreshRefillQuery();
         $("#btnDiv").empty();
         //currentPick = cityInput[0];
         cityInput = [];
@@ -205,147 +204,146 @@ $(document).ready(function () {
 });
 
 function refreshRefillQuery() {
+        buttonProcess();
 
-    buttonProcess();
-
-    //putting in array and making a button and on page
-    function buttonProcess() {
-        cityInput.unshift(currentPick);
-        console.log(cityInput);
-        $(".cityTitle").text(currentPick);
-        $(".cityPickInfo").val("");
-        window.localStorage.setItem('cityInput', JSON.stringify(cityInput));
-        makeButtons();
-    };
+        //putting in array and making a button and on page
+        function buttonProcess() {
+            cityInput.unshift(currentPick);
+            console.log(cityInput);
+            $(".cityTitle").text(currentPick);
+            $(".cityPickInfo").val("");
+            window.localStorage.setItem('cityInput', JSON.stringify(cityInput));
+            makeButtons();
+        };
 
 
-    //getting latitude and logitude from google
-    let queryURLG = `https://maps.googleapis.com/maps/api/geocode/json?address=${currentPick}&key=AIzaSyCRTF7l44EF1KpczSGOLsMu5jewKiBDNW0`
+        //getting latitude and logitude from google
+        let queryURLG = `https://maps.googleapis.com/maps/api/geocode/json?address=${currentPick}&key=AIzaSyCRTF7l44EF1KpczSGOLsMu5jewKiBDNW0`
 
-    $.ajax({
-            url: queryURLG,
-            method: "GET"
-        })
-        .then(function (responseG) {
-            //storing the object in local storage if needed
-            let responseGObj = responseG
-            console.log(responseGObj);
-            window.localStorage.setItem('responseGObj', JSON.stringify(responseGObj));
+        $.ajax({
+                url: queryURLG,
+                method: "GET"
+            })
+            .then(function (responseG) {
+                //storing the object in local storage if needed
+                let responseGObj = responseG
+                console.log(responseGObj);
+                window.localStorage.setItem('responseGObj', JSON.stringify(responseGObj));
+                
+                //finding the coordinates in the response
+                let latitude = responseG.results[0].geometry.location.lat;
+                let lat3 = latitude.toFixed(3);
+                let longitutude = responseG.results[0].geometry.location.lng;
+                let lng3 = longitutude.toFixed(3);
+                $(".cityCoord").text(`Current Data:  (latitute: ${lat3} & longitude: ${lng3})`)
+                console.log(`(latitute: ${lat3} & longitude: ${lng3})`);
+
+
+                //using lat and long to get current weather
+                let queryURLWeather = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitutude}&APPID=56657235d578c7d8b7f0b5ce07c9c887`
+
+                $.ajax({
+                        url: queryURLWeather,
+                        method: "GET"
+                    })
+                    .then(function (responseWeather) {
+
+                        //processing the temp info
+                        let currentTemp = responseWeather.main.temp; 
+                            //changing from Kelvin (0K − 273.15) × 9/5 + 32 = -459.7°F
+                            currentTemp = currentTemp - 273.15;
+                            currentTemp = currentTemp * 9;
+                            currentTemp = currentTemp / 5;
+                            currentTemp = currentTemp + 32;
+                        currentTemp1 = currentTemp.toFixed(1);
+                        $(".currentTempText").text(`Temperature:  ${currentTemp1}°`);
+                        //processing the temp info
+                        let currentWind = responseWeather.wind.speed;
+                        $(".currentwindSpeedText").text(`Wind Speed:   ${currentWind} mph`);
+                        //processing the humidity info
+                        let currentHumid = responseWeather.main.humidity;
+                        $(".currentHumidText").text(`Humidity:    ${currentHumid} %`);
+                        //getting the corect icon from openweathermap
+                        let currentIcon = responseWeather.weather[0].icon;
+                        let iconURL = `http://openweathermap.org/img/wn/${currentIcon}@2x.png`
+                        let $titleImage = $("<img>");
+                        $titleImage.attr("src", iconURL);
+                        $(".cityTitle").append($titleImage);
+                        //adding a decriptions after the icon
+                        let currentConditions = responseWeather.weather[0].description;
+                        let $currentCondtext = $("<h5>");
+                        $currentCondtext.text(currentConditions);
+                        $(".cityTitle").append($titleImage);
+                       
+                    })
+
+                //using lat andd long to get forecast data
+
+                let queryURLForecast = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat3}&lon=${lng3}&APPID=56657235d578c7d8b7f0b5ce07c9c887`
+
+                $.ajax({
+                        url: queryURLForecast,
+                        method: "GET"
+                    })
+                    .then(function (responseForecast) {
             
-            //finding the coordinates in the response
-            let latitude = responseG.results[0].geometry.location.lat;
-            let lat3 = latitude.toFixed(3);
-            let longitutude = responseG.results[0].geometry.location.lng;
-            let lng3 = longitutude.toFixed(3);
-            $(".cityCoord").text(`Current Data:  (latitute: ${lat3} & longitude: ${lng3})`)
-            console.log(`(latitute: ${lat3} & longitude: ${lng3})`);
 
+                        //storing the object in local storage if needed
+                        let responseForecastObj = responseForecast
+                        console.log(responseForecastObj);
 
-            //using lat and long to get current weather
-            let queryURLWeather = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitutude}&APPID=56657235d578c7d8b7f0b5ce07c9c887`
+                        cardProcess();
+                        // //Generating the weather cards
+                        function cardProcess() {
+                            $(".cardDiv").empty();
+                            //weathermap forecast data comes in 3 hour intervals, so pick every 8th one
+                            for (i = 0; i < 40; i=i+8) {
+                                //making the div for each card and connect the Bootstrap format
+                                let $cardSetup = $("<div>");
+                                $cardSetup.addClass(weatherCardformat);
 
-            $.ajax({
-                    url: queryURLWeather,
-                    method: "GET"
-                })
-                .then(function (responseWeather) {
+                                //formatting and setting the date on the card
+                                let futureDate = moment().add(i, "days").format('LL');
+                                let $futureDateH6 = $("<h6>");
+                                $futureDateH6.text(futureDate);
+                                $futureDateH6.addClass(cardTextFormat);
+                                $cardSetup.append($futureDateH6);
 
-                    //processing the temp info
-                    let currentTemp = responseWeather.main.temp; 
-                        //changing from Kelvin (0K − 273.15) × 9/5 + 32 = -459.7°F
-                        currentTemp = currentTemp - 273.15;
-                        currentTemp = currentTemp * 9;
-                        currentTemp = currentTemp / 5;
-                        currentTemp = currentTemp + 32;
-                    currentTemp1 = currentTemp.toFixed(1);
-                    $(".currentTempText").text(`Temperature:  ${currentTemp1}°`);
-                    //processing the temp info
-                    let currentWind = responseWeather.wind.speed;
-                    $(".currentwindSpeedText").text(`Wind Speed:   ${currentWind} mph`);
-                    //processing the humidity info
-                    let currentHumid = responseWeather.main.humidity;
-                    $(".currentHumidText").text(`Humidity:    ${currentHumid} %`);
-                    //getting the corect icon from openweathermap
-                    let currentIcon = responseWeather.weather[0].icon;
-                    let iconURL = `http://openweathermap.org/img/wn/${currentIcon}@2x.png`
-                    let $titleImage = $("<img>");
-                    $titleImage.attr("src", iconURL);
-                    $(".cityTitle").append($titleImage);
-                    //adding a decriptions after the icon
-                    let currentConditions = responseWeather.weather[0].description;
-                    let $currentCondtext = $("<h5>");
-                    $currentCondtext.text(currentConditions);
-                    $(".cityTitle").append($titleImage);
-                   
-                })
+                                //formatting and setting the image on the card
+                                let futureIcon = responseForecast.list[i].weather[0].icon;
+                                let iconURLFuture = `http://openweathermap.org/img/wn/${futureIcon}@2x.png`
+                                let $futureImage = $("<img>");
+                                $futureImage.attr("src", iconURLFuture);
+                                $futureImage.addClass(cardImgFormat);
+                                $cardSetup.append($futureImage);
 
-            //using lat andd long to get forecast data
+                                //formatting and setting the temp on the card
+                                let futureTemp = responseForecast.list[i].main.temp;
+                                futureTemp = futureTemp - 273.15;
+                                futureTemp = futureTemp * 9;
+                                futureTemp = futureTemp / 5;
+                                futureTemp = futureTemp + 32;
+                                futureTemp1 = futureTemp.toFixed(1);
+                                let $futureTempH6 = $("<h6>");
+                                $futureTempH6.text(`Temperature: ${futureTemp1}°`);
+                                $futureTempH6.addClass(cardTextFormat);
+                                $cardSetup.append($futureTempH6);
 
-            let queryURLForecast = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat3}&lon=${lng3}&APPID=56657235d578c7d8b7f0b5ce07c9c887`
+                                //formatting and setting the humidity on the card
+                                let futureHumid = responseForecast.list[i].main.humidity;
+                                let $futureHumidH6 = $("<h6>");
+                                $futureHumidH6.text(` Humidity: ${futureHumid} %`);
+                                $futureHumidH6.addClass(cardTextFormat);
+                                $cardSetup.append($futureHumidH6);
 
-            $.ajax({
-                    url: queryURLForecast,
-                    method: "GET"
-                })
-                .then(function (responseForecast) {
-        
-
-                    //storing the object in local storage if needed
-                    let responseForecastObj = responseForecast
-                    console.log(responseForecastObj);
-
-                    cardProcess();
-                    // //Generating the weather cards
-                    function cardProcess() {
-                        $(".cardDiv").empty();
-                        //weathermap forecast data comes in 3 hour intervals, so pick every 8th one
-                        for (i = 0; i < 40; i=i+8) {
-                            //making the div for each card and connect the Bootstrap format
-                            let $cardSetup = $("<div>");
-                            $cardSetup.addClass(weatherCardformat);
-
-                            //formatting and setting the date on the card
-                            let futureDate = moment().add(i, "days").format('LL');
-                            let $futureDateH6 = $("<h6>");
-                            $futureDateH6.text(futureDate);
-                            $futureDateH6.addClass(cardTextFormat);
-                            $cardSetup.append($futureDateH6);
-
-                            //formatting and setting the image on the card
-                            let futureIcon = responseForecast.list[i].weather[0].icon;
-                            let iconURLFuture = `http://openweathermap.org/img/wn/${futureIcon}@2x.png`
-                            let $futureImage = $("<img>");
-                            $futureImage.attr("src", iconURLFuture);
-                            $futureImage.addClass(cardImgFormat);
-                            $cardSetup.append($futureImage);
-
-                            //formatting and setting the temp on the card
-                            let futureTemp = responseForecast.list[i].main.temp;
-                            futureTemp = futureTemp - 273.15;
-                            futureTemp = futureTemp * 9;
-                            futureTemp = futureTemp / 5;
-                            futureTemp = futureTemp + 32;
-                            futureTemp1 = futureTemp.toFixed(1);
-                            let $futureTempH6 = $("<h6>");
-                            $futureTempH6.text(`Temperature: ${futureTemp1}°`);
-                            $futureTempH6.addClass(cardTextFormat);
-                            $cardSetup.append($futureTempH6);
-
-                            //formatting and setting the humidity on the card
-                            let futureHumid = responseForecast.list[i].main.humidity;
-                            let $futureHumidH6 = $("<h6>");
-                            $futureHumidH6.text(` Humidity: ${futureHumid} %`);
-                            $futureHumidH6.addClass(cardTextFormat);
-                            $cardSetup.append($futureHumidH6);
-
-                            //putting the card on the page
-                            $(".cardDiv").append($cardSetup);
+                                //putting the card on the page
+                                $(".cardDiv").append($cardSetup);
+                            };
                         };
-                    };
-                });
+                    });
 
 
 
-        });
+            });
 
 }
